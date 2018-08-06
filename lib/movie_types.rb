7a322f387f6a1f222495adbedd7f0a1d9bca8e65
Christopher Parsons <mail@christopherparsons.me>
2018-08-06 10:25:47 +0100

    Reward points earned for various movie types calculated

--- lib/movie_types.rb
+++ lib/movie_types.rb
@@ -1,19 +1,34 @@
+# frzoen_string_literal: true
+
 class RegularMovie
-  def get_total(days)
+  def rental_cost(days)
     return 2.0 if days <= 2
     2.0 + ((days - 2) * 1.5)
+  end
+
+  def reward_points(_days)
+    1
   end
 end
 
 class NewMovie
-  def get_total(days)
+  def rental_cost(days)
     3.0 * days
+  end
+
+  def reward_points(days)
+    return 1 if days == 1
+    2
   end
 end
 
 class KidsMovie
-  def get_total(days)
+  def rental_cost(days)
     return 1.5 if days <= 3
     1.5 + ((days - 3) * 1.5)
+  end
+
+  def reward_points(_days)
+    1
   end
 end
--- spec/movie_types_spec.rb
+++ spec/movie_types_spec.rb
@@ -1,35 +1,57 @@
+# frozen_string_literal: true
+
 require 'movie_types'
 
 describe RegularMovie do
-  context 'for a regular movie' do
+  context 'given a number of days' do
     let(:regular) { RegularMovie.new }
     it 'can calculate cost for a one day rental' do
-      expect(regular.get_total(1)).to eq(2.0)
+      expect(regular.rental_cost(1)).to eq(2.0)
     end
 
     it 'can calculate cost for a two day rental' do
-      expect(regular.get_total(2)).to eq(2.0)
+      expect(regular.rental_cost(2)).to eq(2.0)
     end
 
     it 'can calculate cost for a three day rental' do
-      expect(regular.get_total(3)).to eq(3.5)
+      expect(regular.rental_cost(3)).to eq(3.5)
     end
 
     it 'can calculate cost for a four day rental' do
-      expect(regular.get_total(4)).to eq(5)
+      expect(regular.rental_cost(4)).to eq(5)
+    end
+
+    it 'can calculate reward points for a one day rental' do
+      expect(regular.reward_points(1)).to eq(1)
+    end
+
+    it 'can calculate reward points for a twenty day rental' do
+      expect(regular.reward_points(1)).to eq(1)
     end
   end
 end
 
 describe NewMovie do
-  context 'for a new release movie' do
+  context 'given a number of days' do
   let(:new) { NewMovie.new }
     it 'can calculate cost for a one day rental' do
-      expect(new.get_total(1)).to eq(3.0)
+      expect(new.rental_cost(1)).to eq(3.0)
     end
 
     it 'can calculate cost for a two day rental' do
-      expect(new.get_total(2)).to eq(6.0)
+      expect(new.rental_cost(2)).to eq(6.0)
+    end
+
+    it 'can calculate reward points for a one day rental' do
+      expect(new.reward_points(1)).to eq(1)
+    end
+
+    it 'can calculate reward points for a two day rental' do
+      expect(new.reward_points(2)).to eq(2)
+    end
+
+    it 'can calculate reward points for a three day rental' do
+      expect(new.reward_points(3)).to eq(2)
     end
   end
 end
@@ -39,19 +61,27 @@ describe KidsMovie do
     let(:kids) { KidsMovie.new }
 
     it 'can calculate cost for a one day rental' do
-      expect(kids.get_total(1)).to eq(1.5)
+      expect(kids.rental_cost(1)).to eq(1.5)
     end
 
     it 'can calculate cost for a three day rental' do
-      expect(kids.get_total(3)).to eq(1.5)
+      expect(kids.rental_cost(3)).to eq(1.5)
     end
 
     it 'can calculate cost for a four day rental' do
-      expect(kids.get_total(4)).to eq(3.0)
+      expect(kids.rental_cost(4)).to eq(3.0)
     end
 
     it 'can calculate cost for a five day rental' do
-      expect(kids.get_total(5)).to eq(4.5)
+      expect(kids.rental_cost(5)).to eq(4.5)
+    end
+
+    it 'can calculate reward points for a one day rental' do
+      expect(kids.reward_points(1)).to eq(1)
+    end
+
+    it 'can calculate reward points for a twenty day rental' do
+      expect(kids.reward_points(20)).to eq(1)
     end
   end
 end
